4b38ed7a7a5ccc1e149b0a6bcd30b287084a803d
David Bosschaert <david.bosschaert@gmail.com>
2019-12-27 23:43:49 +0000

    WIP

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,42 @@
-// Do we need these?
-// extern crate chrono;
-// extern crate exif;
-// extern crate regex;
+extern crate clap;
+
+use clap::{Arg, ArgMatches, App, SubCommand};
 use phototools::copier::Copier;
 
-use std::env;
 use std::process;
 
+type GenError = Box<dyn std::error::Error>;
+
 fn main() {
+    let matches = App::new("Photo Tools")
+        .version("0.1")
+        .about("Tool to organize photos and videos.")
+        .arg(Arg::with_name("v").short("v").multiple(true)
+            .help("Sets level of verbosity"))
+        .subcommand(SubCommand::with_name("copy")
+            .about("Copies photos and videos from one directory to a target directory \
+                where all the items are organized in target folders based on date taken.")
+            .arg(Arg::with_name("minimum size").short("b").long("min-size").value_name("BYTES")
+                .help("When copying only consider photos and videos of at least this size")
+                .takes_value(true))
+            .arg(Arg::with_name("source directory").short("s").long("source-dir").value_name("DIR")
+                .required(true)
+                .help("The source directory tree")
+                .takes_value(true))
+            .arg(Arg::with_name("destination directory").short("d").long("dest-dir").value_name("DIR")
+                .required(true)
+                .help("The destination directory root")
+                .takes_value(true)))
+        .get_matches();
+
+    // let cfg = Config { from_dir, to_dir };
+    let cfg = Config::from(matches).unwrap_or_else(|err| {
+        println!("Problem initializing with arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Using configuration {:?}", cfg);
+    // run(cfg);
+    /*
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
@@ -16,14 +45,31 @@ fn main() {
     });
 
     run(config);
+    */
 }
 
+#[derive(Debug)]
 struct Config {
     from_dir: String,
-    to_dir: String
+    to_dir: String,
+    min_size: u32
 }
 
 impl Config {
+    fn from(m: ArgMatches) -> Result<Config, GenError> {
+        let min_size_str = m.value_of("min-size").unwrap_or("500");
+        let src_dir = m.value_of("source-dir").unwrap();
+        let dst_dir = m.value_of("dest-dir").unwrap();
+        let min_size = min_size_str.parse::<u32>()?;
+
+        Ok(Config { 
+            from_dir: String::from(src_dir), 
+            to_dir: String::from(dst_dir),
+            min_size
+        })
+    }
+
+    /*
     fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() != 4 {
             return Err("Please provide 3 arguments: (copy|fix_exif_date) <source dir> <target dir>");
@@ -38,6 +84,7 @@ impl Config {
 
         Ok(Config { from_dir, to_dir })
     }
+    */
 }
 
 fn run(config: Config) {
